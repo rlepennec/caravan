f46d65d79964be73a862f62bd928a284e909f634
Kartaj <rlepennec@yahoo.fr>
2023-09-17 17:22:25 +0200

    chuchoteur

--- module/actor/chuchoteur.js
+++ module/actor/chuchoteur.js
@@ -2,6 +2,17 @@ import { CaravanActorSheet } from "./base.js";
 
 export class ChuchoteurSheet extends CaravanActorSheet {
 
+    /** 
+     * @override
+     */
+	static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 800,
+            height: 800,
+            resizable: false
+      });
+    }
+
     /**
      * @return the path of the specified actor sheet.
      */
