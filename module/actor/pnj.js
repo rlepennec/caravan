b4abe306dccaedacb8c3f6aa81db9b3c538e1e1d
Kartaj <rlepennec@yahoo.fr>
2023-09-23 09:01:48 +0200

    pnj

--- module/actor/pnj.js
+++ module/actor/pnj.js
@@ -2,6 +2,17 @@ import { CaravanActorSheet } from "./base.js";
 
 export class PNJSheet extends CaravanActorSheet {
 
+    /** 
+     * @override
+     */
+	static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 800,
+            height: 300,
+            resizable: false
+      });
+    }
+
     /**
      * @return the path of the specified actor sheet.
      */
