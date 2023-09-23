ab22515f4804778a8ac84f6371dbd2d267625f7a
Kartaj <rlepennec@yahoo.fr>
2023-09-23 09:22:23 +0200

    trait

--- module/item/trait.js
+++ module/item/trait.js
@@ -2,6 +2,15 @@ import { CaravanItemSheet } from "./base.js";
 
 export class TraitSheet extends CaravanItemSheet {
 
-
+    /** 
+     * @override
+     */
+	static get defaultOptions() {
+        return mergeObject(super.defaultOptions, {
+            width: 700,
+            height: 220,
+            resizable: true
+      });
+    }
 
 }
