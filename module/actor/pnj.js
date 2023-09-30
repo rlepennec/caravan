611ce159234fba36ed25c31e3aa1ee5681196b19
Kartaj <rlepennec@yahoo.fr>
2023-09-30 20:46:20 +0200

    action

--- module/actor/pnj.js
+++ module/actor/pnj.js
@@ -1,5 +1,5 @@
 import { CaravanActorSheet } from "./base.js";
-import { ActionDialog } from "../dialog/actionDialog.js";
+import { Chat } from "../core/chat.js";
 
 export class PNJSheet extends CaravanActorSheet {
 
@@ -34,8 +34,43 @@ export class PNJSheet extends CaravanActorSheet {
      * @param event The click event.
      */
     async onRollAction(event) {
+
         event.preventDefault();
-        new ActionDialog(this.actor).render(true);
+        const roll = await new Roll(this.actor.system.action + "kh").roll({async: true});
+        const sentence = this.sentenceOf(roll.result);
+
+        await new Chat(this.object)
+            .withTemplate("systems/caravan/templates/chat/action.hbs")
+            .withData({
+                actor: this.actor,
+                result: roll.result,
+                sentence: sentence
+            })
+            .withRoll(roll)
+            .create();
+
+    }
+
+    /**
+     * @param result The roll result between 1 and 6.
+     * @returns the result sentence.
+     */
+    sentenceOf(result) {
+        switch (result) {
+            case "1":
+                return "C'est raté et vous devez payer le prox fort";
+            case "2":
+                return "C'est raté avec un petit revers";
+            case "3":
+                return "C'est raté de justesse ou réussi avec revers";
+            case "4":
+                return "C'est réussi de justesse";
+            case "5":
+                return "C'est réussi avec un peit effet bénéfique";
+            case "6":
+                return "C'est réussi brillamment";
+                
+        }
     }
 
 }
